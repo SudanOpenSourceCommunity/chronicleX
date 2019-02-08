c18910949dbadbe320dc3d8ae5906c26613c3cf0
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2019-02-08 17:53:24 +0300

    Open all APIs docs

--- src/Chronicle/Handlers/Index.php
+++ src/Chronicle/Handlers/Index.php
@@ -61,7 +61,7 @@ class Index implements HandlerInterface
      */
     public function getRoutes(RequestInterface $request): array
     {
-        $allowed = [
+        return [
             [
                 'uri' => '/chronicle/lasthash',
                 'description' => 'Get information about the latest entry in this Chronicle'
@@ -80,47 +80,28 @@ class Index implements HandlerInterface
             ], [
                 'uri' => '/chronicle',
                 'description' => 'API method description'
+            ], [
+                'uri' => '/chronicle/publish',
+                'description' => 'Publish a new message to this Chronicle.',
+                'note' => 'Approved clients only.'
+            ], [
+                'uri' => '/chronicle/register',
+                'description' => 'Approve a new client.',
+                'required-fields' => [
+                    'request-time' => 'The time of your request.',
+                    'publickey' => 'Public key of the client to be removed'
+                ],
+                'note' => 'Administrators only'
+            ], [
+                'uri' => '/chronicle/revoke',
+                'description' => 'Revoke a client\'s access',
+                'required-fields' => [
+                    'request-time' => 'The time of your request.',
+                    'clientid' => 'Unique ID of the client to be removed',
+                    'publickey' => 'Public key of the client to be removed'
+                ],
+                'note' => 'Administrators only'
             ],
         ];
-
-        // Also include these routes if the client has permission:
-        if ($request->hasHeader(Chronicle::CLIENT_IDENTIFIER_HEADER)) {
-            $headers = $request->getHeader(Chronicle::CLIENT_IDENTIFIER_HEADER);
-            $clientId = array_shift($headers);
-            /** @var array<string, string> $dbQueryResult */
-            $dbQueryResult = Chronicle::getDatabase()->row(
-                'SELECT * FROM chronicle_clients WHERE publicid = ?',
-                $clientId
-            );
-            if (!empty($dbQueryResult)) {
-                $allowed []= [
-                    'uri' => '/chronicle/publish',
-                    'description' => 'Publish a new message to this Chronicle.',
-                    'note' => 'Approved clients only.'
-                ];
-                if (!empty($dbQueryResult['isAdmin'])) {
-                    $allowed []= [
-                        'uri' => '/chronicle/register',
-                        'description' => 'Approve a new client.',
-                        'required-fields' => [
-                            'request-time' => 'The time of your request.',
-                            'publickey' => 'Public key of the client to be removed'
-                        ],
-                        'note' => 'Administrators only'
-                    ];
-                    $allowed []= [
-                        'uri' => '/chronicle/revoke',
-                        'description' => 'Revoke a client\'s access',
-                        'required-fields' => [
-                            'request-time' => 'The time of your request.',
-                            'clientid' => 'Unique ID of the client to be removed',
-                            'publickey' => 'Public key of the client to be removed'
-                        ],
-                        'note' => 'Administrators only'
-                    ];
-                }
-            }
-        }
-        return $allowed;
     }
 }
